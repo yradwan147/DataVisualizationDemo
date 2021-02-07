87667e47e9d1556142527bf2bff3234712c36582
yradwan147 <54478004+yradwan147@users.noreply.github.com>
2021-02-07 23:44:22 +0200

    Add files via upload

---
+++ rw_visual.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+while True: 
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+    keep_running = input('Make another walk? (y/n): ')
+    if keep_running == 'n':
+        break
